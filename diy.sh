106da366112385426a90ffe51b3a0862763bdbbf
XCDream <76815376+XCDream@users.noreply.github.com>
2025-10-12 13:06:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -21,5 +21,6 @@
 sed -i 's/192.168.1.1/192.168.1.200/g' package/base-files/files/bin/config_generate
 #sed -i "/helloworld/d" "feeds.conf.default"
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
